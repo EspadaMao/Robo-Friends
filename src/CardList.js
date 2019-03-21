a57e0a251cfe7cf1244970b1236f9e771a045ee5
EspadaMao <mgrianbro-01@hotmail.com>
2019-03-21 19:38:43 +0000

    Fixing the key prop Error

--- src/CardList.js
+++ src/CardList.js
@@ -4,13 +4,17 @@ import Card from './Card';
 import './Card.css';
 
 const CardList = ({robots}) => {
-	const cardsArray = robots.map((user, index) => {
-		return <Card id={robots[index].id} name={robots[index].name} email={robots[index].email}/>;
-	});
 	return (
-
 		<div className='grid-wrapper'>
-			{cardsArray}
+			{
+			robots.map((user, index) => {
+				return (<Card 
+				key={user.id} 
+				id={robots[index].id}
+				name={robots[index].name} 
+				email={robots[index].email}/>);
+	})
+			}
 		</div>
 		);
 }
