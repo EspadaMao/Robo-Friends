29c3673c45380694e8e77bb4cd3f0b37fcaf83d4
EspadaMao <mgrianbro-01@hotmail.com>
2019-03-23 11:53:40 +0000

    Styling my app

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './SearchBox.css';
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange }) => {
 
 	return (
-
-		<div className='search-box-wrapper'>
-			<input className='search-box' type='search' placeholder='search robots'/>
+	<div className='search-box-wrapper'>
+		<div className='search-box'>
+			<input 
+			className='search-txt' 
+			type='search' 
+			placeholder='search for robot friends'
+			onChange={ searchChange }
+			/>
+			<a className='search-btn' href="#"> 
+				<i className="fas fa-search"></i>
+			</a>
 		</div>
-
+	</div>
 	);
 
 }
